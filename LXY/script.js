d7dea8cc448054b55c88853bda7e4eda6aff8284
林宣夜 <88415416+stareyeXuanyeLin@users.noreply.github.com>
2022-08-09 22:43:26 +0800

    Update script.js

--- LXY/script.js
+++ LXY/script.js
@@ -1,21 +1,16 @@
-// 脚本本体
-async function BondageClubLinXuanye() {
+(()=> {
 	// 捕获元素聊天输入框的值
 	var content = ElementValue("InputChat").trim();
-	// 处于聊天室中
-	if (CurrentScreen == "ChatRoom") {
-		if (content.indexOf("/lxy")) {
-			ChatRoomSendLocal("插件安装成功啦~");
+	
+	// 聊天室指令输入
+	function commandLxy(event) {
+		// 处于聊天室中
+		if (CurrentScreen == "ChatRoom" && event.keyCode == 13) {
+			if (content.indexOf("/lxy")) {
+				ChatRoomSendLocal("插件安装成功啦~");
+			}
 		}
 	}
 	
-	var ChatRoomInput = document.getElementById("InputChat");
-	ChatRoomInput.addEventListener("keyup",(event)=>{
-		event.preventDefault();
-		if (event.keyCode == 13) {
-			console.log("插件安装成功~");
-		}
-	});
-}
-
-BondageClubLinXuanye();
+	document.getElementById("InputChat").addEventListener("keyup",commandLxy);
+})();
