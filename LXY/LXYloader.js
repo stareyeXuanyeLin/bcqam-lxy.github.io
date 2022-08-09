e83b112ff88600416110eda4b3ae72524f678a8a
林宣夜 <88415416+stareyeXuanyeLin@users.noreply.github.com>
2022-08-09 20:20:52 +0800

    Add files via upload

---
+++ LXY/LXYloader.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         lxy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  小夜的BondageClub自用插件
+// @author       You
+// @match        https://bondageprojects.elementfx.com/R82/BondageClub/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=elementfx.com
+// @grant        none
+// ==/UserScript==
+
+setTimeout(
+	function () {
+			let n = document.createElement("script");
+			n.setAttribute("language", "JavaScript");
+			n.setAttribute("crossorigin", "anonymous");
+			n.setAttribute("src", "https://stareyexuanyelin.github.io/LXY/script.js?_=" + Date.now());
+			n.onload = () => n.remove();
+			document.head.appendChild(n);
+		}
+);
---
+++ LXY/script.js
@@ -0,0 +1,11 @@
+async function linXuanye() {
+	// 捕获元素聊天输入框的值
+	var content = ElementValue("InputChat").trim();
+	
+	// 处于聊天室中
+	if (CurrentScreen == "ChatRoom") {
+		if (content.indexOf("/lxy") == 0 && content.endsWith("/lxy")) {
+			ChatRoomSendLocal("插件安装成功啦~")
+		}
+	}
+}
