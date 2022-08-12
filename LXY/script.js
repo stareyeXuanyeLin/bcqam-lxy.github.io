8e82d83a303204fd39d4f963ae1479ffb9c9d65c
林宣夜 <88415416+stareyeXuanyeLin@users.noreply.github.com>
2022-08-13 02:04:08 +0800

    Update script.js

--- LXY/script.js
+++ LXY/script.js
@@ -1,22 +1,16 @@
 // 重写原版ChatRoomSendChat方法
 function ChatRoomSendChatNew() {
-	// If there's a message to send
-	// 如果有消息要发送
+	// 获取输入栏文本
 	const msg = ElementValue("InputChat").trim();
-	if (msg != "") {
-		// 检查是否为控制台指令
-		if(msg[0] == "/") {
-			// 去除反斜杠并将首字母转换成大写
-			let inst = msg.substring(1)[0].toUpperCase()+msg.substring(2);
-			// 查找对应的函数并执行
-			InstructionsCall("Instructions"+inst+"Call",msg);
-		}
-		// Keeps the chat log in memory so it can be accessed with pageup/pagedown
-		// 将聊天日志保存在内存中，以便使用pageup/pagedown访问
-		ChatRoomLastMessage.push(msg);
-		ChatRoomLastMessageIndex = ChatRoomLastMessage.length;
-		CommandParse(msg);
+	// 检查是否为控制台指令
+	if(msg[0] == "/") {
+		// 去除反斜杠并将首字母转换成大写
+		let inst = msg.substring(1)[0].toUpperCase()+msg.substring(2);
+		// 查找对应的函数并执行
+		InstructionsCall("Instructions"+inst+"Call",msg);
 	}
+	// 调用旧版方法
+	ChatRoomSendChatUsed();
 }
 
 // 指令调用方法
@@ -30,8 +24,10 @@ function InstructionsCall(funName,...val) {
 // 插件自定义指令
 function InstructionsLxyCall() {
 	ChatRoomSendLocal("插件安装成功啦~");
+	CommandParse("欢迎使用小夜的自定义插件，目前还在施工中呢~");
 }
 
+// 另存旧版方法
+var ChatRoomSendChatUsed = ChatRoomSendChat;
 // 重写原版方法
-var OLDmenu = ChatRoomSendChat;
 var ChatRoomSendChat = ChatRoomSendChatNew;
